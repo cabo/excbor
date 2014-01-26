739c94898b80bab550609eb998763125f38e3449
Carsten Bormann <cabo@tzi.org>
2014-01-26 22:53:27 +0100

    Add expm package descriptor

---
+++ package.head.exs
@@ -0,0 +1,5 @@
+Expm.Package.new(name: "excbor", description: "CBOR (RFC 7049) for Elixir",
+                 version: :head, keywords: ["cbor","json","binary","term"],
+                 maintainers: [[name: "Carsten Bormann",
+                                email: "cabo@tzi.org"]],
+                 repositories: [[github: "cabo/excbor"]])
